0b7682efb2b18b59ea85dc4c10ec2772028cc43a
Alessa Valencia <vector386@gmail.com>
2025-10-28 17:30:02 -0600

    Initial commit

---
+++ src/main/java/com/vikovalfo/hcm_be/HcmBeApplication.java
@@ -0,0 +1,13 @@
+package com.vikovalfo.hcm_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcmBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcmBeApplication.class, args);
+	}
+
+}
