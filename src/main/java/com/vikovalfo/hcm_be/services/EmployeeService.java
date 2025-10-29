6c7e1b25e8562c5d37481fb632b3a712983858ce
Alessa Valencia <vector386@gmail.com>
2025-10-29 11:17:08 -0600

    basic CRUD operations completed

--- src/main/java/com/vikovalfo/hcm_be/services/EmployeeService.java
+++ src/main/java/com/vikovalfo/hcm_be/services/EmployeeService.java
@@ -4,9 +4,11 @@ import com.vikovalfo.hcm_be.models.Employee;
 import com.vikovalfo.hcm_be.models.dtos.EmployeeDto;
 import com.vikovalfo.hcm_be.models.mappers.EmployeeDtoToEmployeeMapper;
 import com.vikovalfo.hcm_be.repositories.EmployeeRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,13 +16,32 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Transactional
     public Employee createNewEmployee(EmployeeDto employeeDto) {
         Employee employee = EmployeeDtoToEmployeeMapper.transform(employeeDto);
         return employeeRepository.save(employee);
     }
 
+    @Transactional
     public Employee getEmployee(Long id) {
         Optional<Employee> employee = employeeRepository.findById(id);
         return employee.get();
+    }
+
+    @Transactional
+    public List<Employee> getEmployees() {
+        List<Employee> list = employeeRepository.findAll();
+        return list;
+    }
+
+    public Employee updateEmployee(Long id, Employee employee) {
+        Optional<Employee> employeeToBeUpdated = employeeRepository.findById(id);
+        employeeToBeUpdated.get().setName(employee.getName());
+        Employee updatedEmployee = employeeRepository.save(employee);
+        return  updatedEmployee;
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
     }
 }
